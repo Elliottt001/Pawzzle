115e6af914bd816fdd766ff3f1d50e7e0d820046
Elliottt001 <rzzhangzju@gmail.com>
2026-01-24 16:37:25 +0800

    改图标

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,6 +1,10 @@
 import { Tabs } from 'expo-router';
 import { IconSymbol } from '../../components/ui/icon-symbol';
 import { PawzzleTabBar } from '../../components/pawzzle-tab-bar';
+import ChatIcon from '@/assets/images/chat.svg';
+import HomeIcon from '@/assets/images/index.svg';
+import PetIcon from '@/assets/images/pet.svg';
+import CommunityIcon from '@/assets/images/square.svg';
 
 export default function TabLayout() {
   return (
@@ -14,8 +18,8 @@ export default function TabLayout() {
         name="index"
         options={{
           title: '首页',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={24} name="house.fill" color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <HomeIcon width={size ?? 24} height={size ?? 24} color={color} />
           ),
         }}
       />
@@ -23,8 +27,8 @@ export default function TabLayout() {
         name="pets"
         options={{
           title: '宠物',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={24} name="pawprint.fill" color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <PetIcon width={size ?? 24} height={size ?? 24} color={color} />
           ),
         }}
       />
@@ -41,8 +45,8 @@ export default function TabLayout() {
         name="community"
         options={{
           title: '广场',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={24} name="person.2.fill" color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <CommunityIcon width={size ?? 24} height={size ?? 24} color={color} />
           ),
         }}
       />
@@ -50,8 +54,8 @@ export default function TabLayout() {
         name="chats"
         options={{
           title: '私聊',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={24} name="message.fill" color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <ChatIcon width={size ?? 24} height={size ?? 24} color={color} />
           ),
         }}
       />
